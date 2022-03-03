494cda7872ae9996ad4792d77667ca7c03d5dac9
Hele-Mai Ristmäe <hristmae@tktk.ee>
2022-03-03 21:34:45 +0200

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti(cpu) ja kasutaja (user);
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem;
+            //mängija, kes viskab rohkem, ongimängu võitja;
+            //*täringuid visatakse kolm korda;
+            //programm kuulutab võitja.
+
+
+            Random rnd = new Random();
+            int cpuScore = 0;
+            int userScore = 0;
+
+            Console.WriteLine("Viska suvalist nuppu vajutades täringuid.");
+            Console.ReadLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                int cpuRandom = rnd.Next(1, 7);
+                int userRandom = rnd.Next(1, 7);
+
+                
+
+                cpuScore = cpuScore + cpuRandom;
+                userScore = userScore + userRandom;
+                
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (i < 2)
+                {
+                    Console.WriteLine("" + "");
+                    Console.WriteLine("Viska täringuid uuesti:");
+                    string userName = Console.ReadLine();
+                }
+                
+            }
+
+                if (cpuScore < userScore)
+                {
+                    Console.WriteLine($"Arvuti täringute summa on {cpuScore}. Kasutaja täringute summa on {userScore}. Kasutaja on mängu võitnud.");
+
+                }
+                else if (cpuScore > userScore)
+                {
+                    Console.WriteLine($"Arvuti täringute summa on {cpuScore}. Kasutaja täringute summa on {userScore}. Arvuti on mängu võitnud.");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Arvuti täringute summa on {cpuScore}. Kasutaja täringute summa on {userScore}. Viik!");
+                }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MoreLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnust ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna "admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast"
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti."
+            //kolm katset
+            int i = 0;
+            
+
+            while (i < 3)
+            {
+                
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta parool:");
+               string userPassword = Console.ReadLine();
+
+                if (userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!!");
+                    break;
+                }
+                else if( i == 2)
+                    {
+                    Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi mõne aja pärast uuesti.");
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi uuesti");
+                }
+
+            }
+           
+
+
+        }
+    }
+}
